c50ed24c481b1bd1def9da8daee30a754d8857d6
Baurjan1234 <baurjanjalgaskhan@gmail.com>
2024-04-29 00:45:21 +0800

    package update

--- src/app/dashboard/post/[title]/page.tsx
+++ src/app/dashboard/post/[title]/page.tsx
@@ -18,8 +18,7 @@ type Props = {
 export default function PostDetailsPage({ params }: Props) {
   const { title } = params;
 
-  // return <PostDetailsView title={title} />;
-  return <div>{title}</div>;
+  return <PostDetailsView title={title} />;
 }
 
 export async function generateStaticParams() {
